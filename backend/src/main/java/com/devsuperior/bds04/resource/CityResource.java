25837bc2c1785722c909f8b03fb4db1d609571bf
GiancarloJr <giancarlo456@hotmail.com>
2022-11-02 18:38:32 -0300

    City Update CRUD

--- backend/src/main/java/com/devsuperior/bds04/resource/CityResource.java
+++ backend/src/main/java/com/devsuperior/bds04/resource/CityResource.java
@@ -4,10 +4,12 @@ import com.devsuperior.bds04.dto.CityDTO;
 import com.devsuperior.bds04.service.CityService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Sort;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import javax.validation.Valid;
 import java.net.URI;
 import java.util.List;
 
@@ -18,16 +20,16 @@ public class CityResource {
     @Autowired
     private CityService cityService;
 
-    @GetMapping("/all")
+    @GetMapping()
     public ResponseEntity<List<CityDTO>> findAll(){
         List<CityDTO> list = cityService.findAll();
         return ResponseEntity.ok().body(list);
     }
-    @PostMapping("/")
-    public ResponseEntity<CityDTO> insert(@RequestBody CityDTO cityDTO){
+    @PostMapping()
+    public ResponseEntity<CityDTO> insert(@Valid @RequestBody CityDTO cityDTO){
         URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
                 .buildAndExpand(cityDTO.getId()).toUri();
-        return ResponseEntity.ok().body(cityService.insert(cityDTO));
+        return ResponseEntity.status(HttpStatus.CREATED).body(cityService.insert(cityDTO));
     }
 
 }
