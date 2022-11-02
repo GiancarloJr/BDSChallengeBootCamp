843c5bb13552d70ed3531bef89bfedcf471f2ee4
GiancarloJr <giancarlo456@hotmail.com>
2022-11-02 00:50:17 -0300

    Validation Exceptions

---
+++ backend/src/main/java/com/devsuperior/bds04/service/exceptions/DataBaseException.java
@@ -0,0 +1,8 @@
+package com.devsuperior.bds04.service.exceptions;
+
+public class DataBaseException extends RuntimeException{
+
+    public DataBaseException(String message) {
+        super(message);
+    }
+}
