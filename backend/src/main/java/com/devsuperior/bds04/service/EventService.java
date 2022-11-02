801b1a706a06179d3171e1fbe09907165b85d98b
GiancarloJr <giancarlo456@hotmail.com>
2022-11-02 00:49:49 -0300

    Event Service and Update SQL Import

--- backend/src/main/java/com/devsuperior/bds04/service/EventService.java
+++ backend/src/main/java/com/devsuperior/bds04/service/EventService.java
@@ -1,4 +1,26 @@
 package com.devsuperior.bds04.service;
 
+import com.devsuperior.bds04.dto.CityDTO;
+import com.devsuperior.bds04.dto.EventDTO;
+import com.devsuperior.bds04.entities.City;
+import com.devsuperior.bds04.entities.Event;
+import com.devsuperior.bds04.repository.CityRepository;
+import com.devsuperior.bds04.repository.EventRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class EventService {
+
+    @Autowired
+    private EventRepository eventRepository;
+
+    public List<EventDTO> findAll(Long id) {
+        List<EventDTO> listDTO = new ArrayList<>();
+        for (Event entity : eventRepository.findAll()) {
+            listDTO.add(new EventDTO(entity));
+        }
+        return listDTO;
+    }
 }
